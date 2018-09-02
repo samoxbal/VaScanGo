b1fd7c7a796fb3a7a1b19d64d49b70be789abd3b
Dmitry Samokhvalov <dsamokhvalov@alfabank.ru>
2018-09-02 16:34:25 +0300

    Add graphql controller

---
+++ controllers/graphql.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"github.com/go-bongo/bongo"
+	"github.com/kataras/iris"
+)
+
+func GraphqlController(connection *bongo.Connection) iris.Handler {
+	return func(ctx iris.Context) {
+
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"github.com/go-bongo/bongo"
+	"github.com/kataras/iris"
+	"VaScanGo/controllers"
+)
 
 func main() {
 	app := iris.New()
+	bongoConfig := &bongo.Config{
+		ConnectionString: "localhost",
+		Database:         "vascan",
+	}
+	connection, err := bongo.Connect(bongoConfig)
+	if err != nil {
+
+	}
+	app.Post("/graphql", controllers.GraphqlController(connection))
 	app.Run(
 		iris.Addr(":8080"),
 		iris.WithConfiguration(iris.TOML("./config/iris.toml")),
